ce364c86610221436a4328d6973cd2cf1e2ba365
fujimotohiroki <59870212+fujimotohiroki@users.noreply.github.com>
2020-02-14 15:46:09 +0900

    マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
-    def index
+
+    def show
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'logout/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
-  resources :users, only: :index
+  resources :users, only: [:show]
+
 end
