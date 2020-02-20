4d8423e08bf6ea2292d4b3f2b9a55c43f302d77e
saitoaki168 <saitoaki168@users.noreply.github.com>
2020-02-20 21:07:36 +0900

    2st

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,16 +7,16 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
         if @category.has_children? && @category.has_parent?
             @categories = @category.children
-            @categoryname = @category.child_ids
-            @item = Product.where(category_id: @categoryname).order("created_at DESC").page(params[:page]).per(20)
+            @category_ids = @category.child_ids
+            @item = Product.where(category_id: @category_ids).order("created_at DESC").page(params[:page]).per(20)
         elsif @category.is_childless? 
-            @categoryname = @category.sibling_ids
-            @categories = Category.find(@categoryname)
+            @category_ids = @category.sibling_ids
+            @categories = Category.find(@category_ids)
             @item = @category.products.order("created_at DESC").page(params[:page]).per(20)
         else
             @categories = @category.children
-            @categoryname = @category.indirect_ids
-            @item = Product.where(category_id: @categoryname).order("created_at DESC").page(params[:page]).per(20)
+            @category_ids = @category.indirect_ids
+            @item = Product.where(category_id: @category_ids).order("created_at DESC").page(params[:page]).per(20)
         end
     end
 
