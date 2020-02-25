8b412b221a11714e698359bbca77d29f6fc99df2
saitoaki168 <saitoaki168@users.noreply.github.com>
2020-02-25 20:19:58 +0900

    1st

---
+++ app/models/address.rb
@@ -0,0 +1,8 @@
+class Address < ApplicationRecord
+  belongs_to :user
+  
+  validates :address_number, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :town, presence: true
+end
